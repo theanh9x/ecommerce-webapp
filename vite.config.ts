4e31428ea9b193481dc3dd4d0b88c188515fc49a
theanh9x <theanh4497@gmail.com>
2026-01-07 22:57:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
+  /*optimizeDeps: {
     exclude: ['lucide-react'],
-  },
+  },*/
   base: '/ecommerce-webapp/'
 });
